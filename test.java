0bde76a2e768fd637604053f74f8233b835fd559
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2025-10-02 15:16:30 +0700

    user 2 merubah kode

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("ini baris pertama user 3 mengubah diubah user1");
-        System.out.println("ini baris kedua user 3 mengubah diubah user1");
-        System.out.println("ini baris ketiga user 3 mengubah diubah oleh user1");
+        System.out.println("ini baris pertama user 2 mengubah diubah user1");
+        System.out.println("ini baris kedua user 2 mengubah diubah user1");
+        System.out.println("ini baris ketiga user 2 mengubah diubah oleh user1");
     }
 }
