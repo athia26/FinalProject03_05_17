787d8d01901782118bc56d71f2915a032bf7b2a1
athia26 <widas22.athia@gmail.com>
2025-10-02 15:16:00 +0700

    user1 mengubah file

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("ini baris pertama user 3 mengubah diubah user1");
-        System.out.println("ini baris kedua user 3 mengubah diubah user1");
-        System.out.println("ini baris ketiga user 3 mengubah diubah oleh user1");
+        System.out.println("ini baris pertama user 3 mengubah diubah user2");
+        System.out.println("ini baris kedua user 3 mengubah diubah user2");
+        System.out.println("ini baris ketiga user 3 mengubah diubah oleh user2");
     }
 }
