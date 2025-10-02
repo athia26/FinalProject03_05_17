d18584d2fe7a69d0cffa389ebd087d461bf814e8
alyamukhbita <alyamukhbita237@gmail.com>
2025-10-02 15:11:02 +0700

    user 3 mengubah isi baris

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("ini baris pertama");
-        System.out.println("ini baris kedua");
-        System.out.println("ini baris ketiga");
+        System.out.println("ini baris pertama user 3 mengubah");
+        System.out.println("ini baris kedua user 3 mengubah");
+        System.out.println("ini baris ketiga user 3 mengubah");
     }
 }
